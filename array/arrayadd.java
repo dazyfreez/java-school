c30a7ad10db6f7540e625cb153a9ca834cff1e4f
dazyfreez <74270036+dazyfreez@users.noreply.github.com>
2022-05-06 19:22:25 +0200

    -

--- array/arrayadd.java
+++ array/arrayadd.java
@@ -5,7 +5,7 @@ public class arrayadd {
         System.out.print("enter a number from 0 to 4");
         int x = sc.nextInt();
         if (x<5){
-            int[] a = {1, 2, 3, 4, 5};
+        int[] a = {1, 2, 3, 4, 5};
         int[] b = {6, 7, 8, 9, 10};
         int[] c = new int[a.length + b.length];
         for (int i= 0; i < a.length; i++) {
