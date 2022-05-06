66cf337bcec659a1c784192b467c8ae8b4910cdd
dazyfreez <74270036+dazyfreez@users.noreply.github.com>
2022-05-06 18:25:37 +0200

    commit für main

--- math.java
+++ math.java
@@ -19,4 +19,24 @@ public class math {
         }
         return erg;
     }
+
+    public int fakultät( int num) { // Fakultät so machen wir es in der Schule
+        int erg = 1;
+        for (int i = 1; i <= num; i++) {
+            erg = erg * i;
+        }
+        return erg;
+    }
+
+    public static void main(String[] args) { // Das ist fakultät mit der mainmethode
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num = sc.nextInt();
+        int erg = 1;
+        for (int i = 1; i <= num; i++) {
+            erg = erg * i;
+        }
+        System.out.println("The nunber is " + erg);
+        sc.close();
+    }
 }
