cff9b7912e626c8e4f71651116f27099167db72b
dazyfreez <74270036+dazyfreez@users.noreply.github.com>
2022-05-06 19:12:51 +0200

    -

---
+++ array/arrayadd.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class arrayadd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter a number from 0 to 4");
+        int x = sc.nextInt();
+        if (x<5){
+            int[] a = {1, 2, 3, 4, 5};
+        int[] b = {6, 7, 8, 9, 10};
+        int[] c = new int[a.length + b.length];
+        for (int i= 0; i < a.length; i++) {
+            c[i] = a[i] + b[i];
+        }
+        System.out.println(c[x]);
+        }
+        else{
+            System.out.println("the array is not that large");
+        }
+        sc.close();
+    }
+    
+    
+    
+}
