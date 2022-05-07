d3bd5ac1bdfb5865518c2d711539c03addb01ffe
dazyfreez <74270036+dazyfreez@users.noreply.github.com>
2022-05-07 15:14:32 +0200

    .

--- array/combiarray.java
+++ array/combiarray.java
@@ -1,6 +1,6 @@
 public class combiarray {
     public static void main(String[] args) {
-        int[] a = {1, 2, 3, 4, 5};
+        int[] a = {1, 2, 3, 0, 5};
         int[] b = {6, 7, 8, 3, 10};
         int erg = b[0]; 
         int erg1 = a[0];
