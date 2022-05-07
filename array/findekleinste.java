d3f782b7af18db282aaabdd332638ebd1ff305b3
dazyfreez <74270036+dazyfreez@users.noreply.github.com>
2022-05-07 11:49:47 +0200

    -

--- array/findekleinste.java
+++ array/findekleinste.java
@@ -13,4 +13,17 @@ public class findekleinste{
         
     return erg;
     }
+
+    public static void main(String[] args) {
+       int erg = f[0];
+            for(int i=1; i<f.length; i++)
+            {
+                if (f[i]< erg)
+                {
+                    erg = f[i];
+                }
+            }
+            System.out.println(erg);
+       
+    }
 }
