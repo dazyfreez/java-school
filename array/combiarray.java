e224f90f4c30d2291104d30fb8010e3b543f5331
dazyfreez <74270036+dazyfreez@users.noreply.github.com>
2022-05-07 14:30:15 +0200

    '

--- array/combiarray.java
+++ array/combiarray.java
@@ -1,3 +1,18 @@
 public class combiarray {
-    
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3, 4, 5};
+        int[] b = {6, 7, 8, 9, 10};
+        int erg = b[0]; 
+        int erg1 = a[0];
+        for (int i = 1; i <a.length; i++) {
+            if (a[i]<erg1) {
+                erg1 = a[i];
+            }
+            if (b[i]<erg) {
+                erg = b[i];
+            }
+        }
+        System.out.println(erg+" "+erg1);
+
+}
 }
