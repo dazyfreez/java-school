2d854bd77e22ccd3e02212a752a926a0392467bd
dazyfreez <74270036+dazyfreez@users.noreply.github.com>
2022-05-08 14:23:38 +0200

    -

--- java 10d/math/hochrechnen.java
+++ java 10d/math/hochrechnen.java
@@ -1,12 +1,16 @@
 package math;
+import java.util.Scanner;
 public class hochrechnen {
     public static void main(String[] args) {
-        int n= 4;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Bitte geben Sie eine Zahl ein:");
+        int n= sc.nextInt();
         int erg = 0;
       
       for (int i =1; i <=n; i++) {
           erg = erg + i;
       } 
       System.out.println(erg);
+      sc.close();
     }
 }
