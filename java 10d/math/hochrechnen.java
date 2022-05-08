33a2d3c85206137e1cb1aed879677e2c7a20361e
dazyfreez <74270036+dazyfreez@users.noreply.github.com>
2022-05-08 14:21:26 +0200

    +

--- java 10d/math/hochrechnen.java
+++ java 10d/math/hochrechnen.java
@@ -2,7 +2,7 @@ package math;
 public class hochrechnen {
     public static void main(String[] args) {
         int n= 4;
-        int erg = 1;
+        int erg = 0;
       
       for (int i =1; i <=n; i++) {
           erg = erg + i;
